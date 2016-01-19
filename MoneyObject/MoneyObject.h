25f5bbdedffd92a5171f6fe0380066e5125949c1
Sven Günther <sven.guenther@it-agile.de>
2016-01-19 17:09:15 +0100

    Initial Commit

---
+++ MoneyObject/MoneyObject.h
@@ -0,0 +1,19 @@
+//
+//  MoneyObject.h
+//  MoneyObject
+//
+//  Created by Sven Günther on 19.01.16.
+//  Copyright © 2016 Sven Günther. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MoneyObject.
+FOUNDATION_EXPORT double MoneyObjectVersionNumber;
+
+//! Project version string for MoneyObject.
+FOUNDATION_EXPORT const unsigned char MoneyObjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoneyObject/PublicHeader.h>
+
+
